373c5c248eb90420f57a00f3626a100d284b0e38
varol <murat.varol@kgteknoloji.com>
2019-06-29 21:03:03 +0300

    getNews and getSources methods added.

--- app/src/main/java/com/varol/boynews/remote/Api.kt
+++ app/src/main/java/com/varol/boynews/remote/Api.kt
@@ -1,6 +1,23 @@
 package com.varol.boynews.remote
 
+import com.varol.boynews.models.NewsModel
+import com.varol.boynews.models.SourceModel
+import io.reactivex.Single
+import retrofit2.http.POST
+import retrofit2.http.Query
+
 interface Api {
+
+    @POST("/v2/sources")
+    fun getSources(
+        @Query(value = "apiKey") apiKey: String
+    ): Single<BaseResponse<SourceModel>>
+
+    @POST("v2/top-headlines")
+    fun getTopHeadLines(
+        @Query(value = "apiKey") apiKey: String,
+        @Query(value = "sources") sources: String
+    ): Single<BaseResponse<MutableList<NewsModel>>>
 
 
 }
