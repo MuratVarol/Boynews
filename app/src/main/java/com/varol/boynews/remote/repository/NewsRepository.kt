adf7092a238190761a94970ce782347f1ad8ee9a
varol <murat.varol@kgteknoloji.com>
2019-06-29 21:05:11 +0300

    NewsRepository added.

---
+++ app/src/main/java/com/varol/boynews/remote/repository/NewsRepository.kt
@@ -0,0 +1,26 @@
+package com.varol.boynews.remote.repository
+
+import com.varol.boynews.base.BaseRepository
+import com.varol.boynews.base.service
+import com.varol.boynews.models.NewsModel
+import com.varol.boynews.models.SourceModel
+import com.varol.boynews.remote.Api
+import com.varol.boynews.remote.BaseResponse
+import com.varol.boynews.remote.DataHolder
+import io.reactivex.Single
+
+class NewsRepository(val api: Api) : BaseRepository() {
+
+    fun getSources(apiKey: String): Single<DataHolder<BaseResponse<SourceModel>>> {
+        return service.sendRequest(
+            api.getSources(apiKey)
+        )
+    }
+
+    fun getNews(apiKey: String, source: String): Single<DataHolder<BaseResponse<MutableList<NewsModel>>>> {
+        return service.sendRequest(
+            api.getTopHeadLines(apiKey, source)
+        )
+    }
+
+}
