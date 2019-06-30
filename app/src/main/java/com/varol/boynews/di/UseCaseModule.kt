e7f6a2ff0a04a9edd9d6fd1c2a6ca44113cb869a
varol <murat.varol@kgteknoloji.com>
2019-06-30 22:41:44 +0300

    NewsMappingUseCase added to module

--- app/src/main/java/com/varol/boynews/di/UseCaseModule.kt
+++ app/src/main/java/com/varol/boynews/di/UseCaseModule.kt
@@ -2,10 +2,12 @@ package com.varol.boynews.di
 
 import com.varol.boynews.usecase.GetNewsUseCase
 import com.varol.boynews.usecase.GetSourcesUseCase
+import com.varol.boynews.usecase.NewsMappingUseCase
 import org.koin.dsl.module.Module
 import org.koin.dsl.module.module
 
 val useCaseModule: Module = module {
     single { GetSourcesUseCase(get(), get(name = "apiKey")) }
     single { GetNewsUseCase(get(), get(name = "apiKey")) }
+    single { NewsMappingUseCase() }
 }
