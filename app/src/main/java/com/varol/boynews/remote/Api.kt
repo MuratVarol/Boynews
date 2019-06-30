2e4eca4d6356f9e5ae6242b7f801581a6a1a4f91
varol <murat.varol@kgteknoloji.com>
2019-06-30 22:37:46 +0300

    link fixed for sources

--- app/src/main/java/com/varol/boynews/remote/Api.kt
+++ app/src/main/java/com/varol/boynews/remote/Api.kt
@@ -8,7 +8,7 @@ import retrofit2.http.Query
 
 interface Api {
 
-    @GET("/v2/sources")
+    @GET("v2/sources")
     fun getSources(
         @Query(value = "apiKey") apiKey: String
     ): Single<BaseSourceResponse<MutableList<SourceModel>>>
