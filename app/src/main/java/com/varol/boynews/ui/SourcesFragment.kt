b8f2257a660dfa3dcb34da7cfa627d0242729c50
varol <murat.varol@kgteknoloji.com>
2019-06-30 16:48:18 +0300

    fragment created

---
+++ app/src/main/java/com/varol/boynews/ui/SourcesFragment.kt
@@ -0,0 +1,27 @@
+package com.varol.boynews.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.varol.boynews.R
+import com.varol.boynews.base.BaseFragment
+import com.varol.boynews.databinding.FragmentNewsListBinding
+import com.varol.boynews.viewmodel.NewsVM
+
+class SourcesFragment  : BaseFragment<NewsVM,FragmentNewsListBinding>(NewsVM::class) {
+    override val getLayoutId: Int = R.layout.fragment_sources_list
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        viewModel.getAllSources()
+
+        return binding.root
+    }
+}
