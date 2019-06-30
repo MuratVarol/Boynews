dc1a834d225539d4ccb70a75e044ff4846356b34
varol <murat.varol@kgteknoloji.com>
2019-06-30 16:50:49 +0300

    getSources method added

--- app/src/main/java/com/varol/boynews/viewmodel/NewsVM.kt
+++ app/src/main/java/com/varol/boynews/viewmodel/NewsVM.kt
@@ -5,19 +5,27 @@ import androidx.lifecycle.MutableLiveData
 import com.varol.boynews.base.BaseVM
 import com.varol.boynews.models.NewsModel
 import com.varol.boynews.models.SourceModel
+import com.varol.boynews.remote.DataHolder
+import com.varol.boynews.usecase.GetNewsUseCase
+import com.varol.boynews.usecase.GetSourcesUseCase
 import com.varol.boynews.util.listener.ItemClickListener
 
-class NewsVM : BaseVM() {
-    val dummy = MutableLiveData<String>()
+class NewsVM(
+    private val getNewsUseCase: GetNewsUseCase,
+    private val getSourcesUseCase: GetSourcesUseCase
+) : BaseVM() {
 
-    val newsList = MutableLiveData<NewsModel>()
-    val sourcesList = MutableLiveData<SourceModel>()
+    val newsList = MutableLiveData<MutableList<NewsModel>>()
+    val sourcesList = MutableLiveData<MutableList<SourceModel>>()
+    val selectedNews = MutableLiveData<NewsModel>()
+    val selectedSource = MutableLiveData<SourceModel>()
 
 
     val newsClickListener: ItemClickListener<NewsModel> = object
         : ItemClickListener<NewsModel> {
         override fun onItemClick(view: View, item: NewsModel, position: Int) {
 
+            selectedNews.postValue(item)
 
         }
     }
@@ -26,7 +34,33 @@ class NewsVM : BaseVM() {
         : ItemClickListener<SourceModel> {
         override fun onItemClick(view: View, item: SourceModel, position: Int) {
 
+            selectedSource.postValue(item)
 
         }
-}
+    }
+
+
+    fun getAllSources() {
+        val disposable = getSourcesUseCase
+            .getSources()
+            .observeOn(getBackgroundScheduler())
+            .subscribeOn(getBackgroundScheduler())
+            .subscribe({ data ->
+
+                when (data) {
+                    is DataHolder.Success -> {
+
+                        if (data.data.status == "ok")
+                            sourcesList.postValue(data.data.sources)
+
+                    }
+                    is DataHolder.Error -> {
+                        errorMessage.postValue("Failed to download sources.")
+                    }
+                }
+            }, {
+                errorMessage.postValue(it.toString())
+            })
+        disposables.add(disposable)
+    }
 }
