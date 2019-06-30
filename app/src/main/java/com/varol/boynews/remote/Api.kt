e0d9115474551ff675c66cb8630596eefdc5b300
varol <murat.varol@kgteknoloji.com>
2019-06-30 18:01:21 +0300

    post to get

--- app/src/main/java/com/varol/boynews/remote/Api.kt
+++ app/src/main/java/com/varol/boynews/remote/Api.kt
@@ -3,17 +3,17 @@ package com.varol.boynews.remote
 import com.varol.boynews.models.NewsModel
 import com.varol.boynews.models.SourceModel
 import io.reactivex.Single
-import retrofit2.http.POST
+import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface Api {
 
-    @POST("/v2/sources")
+    @GET("/v2/sources")
     fun getSources(
         @Query(value = "apiKey") apiKey: String
     ): Single<BaseSourceResponse<MutableList<SourceModel>>>
 
-    @POST("v2/top-headlines")
+    @GET("v2/top-headlines")
     fun getTopHeadLines(
         @Query(value = "apiKey") apiKey: String,
         @Query(value = "sources") sources: String
