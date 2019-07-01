f56ba065b1c18321bf9f60f4351058dbd02bf738
varol <murat.varol@kgteknoloji.com>
2019-07-01 11:27:47 +0300

    inform message observer added

--- app/src/main/java/com/varol/boynews/ui/MainActivity.kt
+++ app/src/main/java/com/varol/boynews/ui/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.varol.boynews.ui
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.varol.boynews.R
 import com.varol.boynews.base.BaseActivity
 import com.varol.boynews.databinding.ActivityMainBinding
+import com.varol.boynews.extension.informToast
 import com.varol.boynews.viewmodel.MainVM
+import observe
 
 class MainActivity : BaseActivity<MainVM,ActivityMainBinding>(MainVM::class) {
 
@@ -13,7 +14,24 @@ class MainActivity : BaseActivity<MainVM,ActivityMainBinding>(MainVM::class) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        subscribeErrorMessage()
+        subscribeInformMessage()
+
         loadFragment(R.id.container_main, SourcesFragment(),false)
 
+    }
+
+    private fun subscribeInformMessage() {
+        viewModel.informMessage.observe(this) {
+            informToast(it.toString())
+        }
+    }
+
+
+    private fun subscribeErrorMessage() {
+        viewModel.informMessage.observe(this) {
+            informToast(it.toString())
+        }
     }
 }
